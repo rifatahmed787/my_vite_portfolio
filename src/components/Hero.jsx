d42b1f034a3772ba072c107e1bef34abd5e220bc
Rifat <mdrifatahmed787@gmail.com>
2023-03-31 22:30:02 +0600

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,11 @@
 import { motion } from "framer-motion";
-
+import { image } from "../assets";
 import { styles } from "../styles";
+import ReactTypingEffect from "react-typing-effect";
+import resume from "../../public/pdf file/Rifat-resume.pdf"
+import { Icon } from '@iconify/react';
+
+
 
 
 const Hero = () => {
@@ -16,13 +21,52 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
+            Hi, I'm <span className='text-[#915EFF]'>Rifat</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+          <ReactTypingEffect
+              className="text-2xl font-bold"
+              text={[
+                " a Front-End Web Developer",
+                " a MERN Stack Developer",
+                " a Full Stack Developer",
+              ]}
+              cursorRenderer={(cursor) => <h1>{cursor}</h1>}
+              displayTextRenderer={(text, i) => {
+                return (
+                  <h1 className="mt-5">
+                    <span className="text-3xl font-bold">
+                      I am
+                    </span>
+                    {text.split("").map((char, i) => {
+                      const key = `${i}`;
+                      return (
+                        <span key={key} className="text-teal-500">
+                          {char}
+                        </span>
+                      );
+                    })}
+                  </h1>
+                );
+              }}
+            />
           </p>
+         <div className="lg:flex py-10">
+            <button className="flex items-center border-2 border-indigo-500 px-4 py-2 mr-7"> 
+            <Icon icon="la:hire-a-helper" width="25" className="mr-2"/>
+              Hire Me
+            </button>
+            <button className=" border-2 hover:bg-indigo-500 hover:text-white border-indigo-500 px-4 py-2">
+            <a href={resume} download target="_blank" className="flex items-center">
+            <Icon icon="material-symbols:download-rounded" width="25" className="mr-2"/>
+              Download Resume
+            </a>  
+            </button>
+         </div>
         </div>
+        <div className="mx-auto">
+        <img src={image} alt='image' className='w-80 object-contain border rounded-full hidden lg:block' />
+          </div>
       </div>
 
 
